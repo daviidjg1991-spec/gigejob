3bb570aa7926fc6c1671c45d265b996d8cd719cd
daviidjg1991-spec <daviidjg1991@gmail.com>
2026-07-27 15:13:51 +0200

    feat: completar subida del backend cloud functions y build de frontend

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,41 +1,5 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notifyAccepted = exports.notifyRequest = void 0;
-const functions = __importStar(require("firebase-functions/v2"));
-const nodemailer = __importStar(require("nodemailer"));
+import * as functions from "firebase-functions/v2";
+import * as nodemailer from "nodemailer";
 // Leer la contraseña desde el entorno o Secret Manager
 const SMTP_USER = process.env.SMTP_USER || "info@gigejob.com";
 const SMTP_PASS = process.env.SMTP_PASS || "";
@@ -49,7 +13,7 @@ const transporter = nodemailer.createTransport({
         pass: SMTP_PASS,
     },
 });
-exports.notifyRequest = functions.https.onRequest({ cors: true }, async (req, res) => {
+export const notifyRequest = functions.https.onRequest({ cors: true }, async (req, res) => {
     if (req.method !== "POST") {
         res.status(405).send("Method Not Allowed");
         return;
@@ -95,7 +59,7 @@ exports.notifyRequest = functions.https.onRequest({ cors: true }, async (req, re
         res.status(500).json({ error: "Error sending email" });
     }
 });
-exports.notifyAccepted = functions.https.onRequest({ cors: true }, async (req, res) => {
+export const notifyAccepted = functions.https.onRequest({ cors: true }, async (req, res) => {
     if (req.method !== "POST") {
         res.status(405).send("Method Not Allowed");
         return;
