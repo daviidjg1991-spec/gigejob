6599df7bdc17c2d958b67fd9144d653eb74b9b1c
daviidjg1991-spec <daviidjg1991@gmail.com>
2026-09-21 14:54:21 +0200

    style: Increase size of app download badges

--- src/App.tsx
+++ src/App.tsx
@@ -28354,7 +28354,7 @@ function App() {
                             rel="noopener noreferrer"
                             className="hover:opacity-80 transition-opacity hover:-translate-y-0.5 duration-200 block"
                           >
-                            <img src="/app-store-badge.png" alt="Download on the App Store" className="h-10 sm:h-12 w-auto object-contain" />
+                            <img src="/app-store-badge.png" alt="Download on the App Store" className="h-12 sm:h-14 w-auto object-contain" />
                           </a>
                         )}
                         {footerConfig.appDownloads.android && (
@@ -28364,7 +28364,7 @@ function App() {
                             rel="noopener noreferrer"
                             className="hover:opacity-80 transition-opacity hover:-translate-y-0.5 duration-200 block"
                           >
-                            <img src="/google-play-badge.png" alt="Get it on Google Play" className="h-10 sm:h-12 w-auto object-contain" />
+                            <img src="/google-play-badge.png" alt="Get it on Google Play" className="h-12 sm:h-14 w-auto object-contain" />
                           </a>
                         )}
                         {footerConfig.appDownloads.huawei && (
@@ -28374,7 +28374,7 @@ function App() {
                             rel="noopener noreferrer"
                             className="hover:opacity-80 transition-opacity hover:-translate-y-0.5 duration-200 block"
                           >
-                            <img src="/appgallery-badge.png" alt="Explore it on AppGallery" className="h-10 sm:h-12 w-auto object-contain" />
+                            <img src="/appgallery-badge.png" alt="Explore it on AppGallery" className="h-12 sm:h-14 w-auto object-contain" />
                           </a>
                         )}
                       </div>
